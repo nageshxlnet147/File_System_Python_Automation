0857828237f885652b371d0eab4d55312bc2d4da
Nagesh <test@gmail.com>
2024-12-04 21:35:06 +0530

    Python_Automation_Code

---
+++ Python_Automation/data_should_be_formated_in_folder_inside_files.py
@@ -0,0 +1,32 @@
+import os
+import shutil
+source_path = '/home/Python_Automation'         # Parent path
+
+
+dt = {
+    'language':['Python', 'C', 'Java', 'Go'],
+    'Laptop' : ['LG', 'Samsung', 'Asus','Lenovo','Dell', 'HP'],
+    'Cost':['ONE','TWO','THREE','FOUR']
+}
+try:
+    folder_rename_count = 1
+    for i, j in dt.items():
+        if i != False:
+            folder_assigning = folder_rename_count
+            df = str(folder_assigning)+'_'+ i   # Adding the number of folders to be counted in front
+            f = os.path.join(source_path, df)    # join the source path and keys
+            os.makedirs(f, exist_ok = True)     # Creating the f paths
+            file_rename_count = 1
+            for k in j:
+                assigning = file_rename_count
+                p = os.path.join(source_path,  k)   # Join the source path and value k
+                if os.path.exists(p):
+                    new_file_name = str(assigning) + '_' + k    # Adding the number of files to be counted in front
+                    new_file_name_path = os.path.join(source_path,  new_file_name)  #join the source path and new_file_name
+                    os.rename(p, new_file_name_path)    # Rename the old path to new path
+                    shutil.move(new_file_name_path, f)  # moving the new path data to destination path 
+                    print('copied')
+                    file_rename_count += 1  # Increasing the rename files count
+        folder_rename_count += 1    # Increasing the rename folder count
+except Exception as e:
+    print(e)
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # File_System_Python_Automation
+Steps to Follow:
+        1. Source Path required
+        2. Data required in dictionary format
+        3. Identify the keys and values
+        4. Join the source and destination path
+        5. Create the destination path
+        6. Move the data to destination path based on your format modifications
+        7. If above steps not to be work then print the Exception error.    
